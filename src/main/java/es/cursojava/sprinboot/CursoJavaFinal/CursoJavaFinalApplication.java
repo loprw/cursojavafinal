2fc783ccc2de1b689a95f8b4cf3fdcc9dedd0e2b
loprw <loprwdemileto@gmail.com>
2025-05-26 19:49:44 +0200

    Initial commit

---
+++ src/main/java/es/cursojava/sprinboot/CursoJavaFinal/CursoJavaFinalApplication.java
@@ -0,0 +1,13 @@
+package es.cursojava.sprinboot.CursoJavaFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoJavaFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoJavaFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/cursojava/sprinboot/CursoJavaFinal/CursoJavaFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package es.cursojava.sprinboot.CursoJavaFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoJavaFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
